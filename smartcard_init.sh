4916c7c2593e2938cbed8f638392386452e22756
Elizabeth Ferdman <gnudevliz@gmail.com>
2017-01-29 23:11:45 -0800

    decompose whiptail tui code

--- smartcard_init.sh
+++ smartcard_init.sh
@@ -1,80 +1,82 @@
 #!/bin/bash
-
+#
 # smartcard-init.sh initializes the smartcard for the user
 # Prompts for Surname, Given Name, Language Preferences, Login, 
 # Admin PIN, User PIN, and Reset Code
 
 set -a
+set -e
+
+# keep this for testing
+. init.sh
+. whiptail_helper.sh
 
 # Ask if the user will be using a smartcard
-whiptail --yesno "Would you like to send your keys to a smartcard?" 8 78 --title "Smart Card"
-
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
+if whiptail_yesno "SMARTCARD_YESNO_TEXT" "SMARTCARD_YESNO_TITLE"; then
 	
 	# SMARTCARD INITIALIZATION
-	if (whiptail --yesno "Would you like to initialize a smartcard with your name, language, and login?" 8 78 --title "Smart Card Initialization") then
+	if whiptail_yesno "SMARTCARD_INIT_YESNO_TEXT" "SMARTCARD_INIT_YESNO_TITLE"; then
 		
-		SMARTCARD_SURNAME=$(whiptail --inputbox "Please enter the Cardholder's Surname" 8 78 --title "Cardholder's Surname" 3>&1 1>&2 2>&3)
+		whiptail_input "SMARTCARD_SURNAME" "SMARTCARD_SURNAME_TEXT" "SMARTCARD_SURNAME_TITLE"
 		
-		SMARTCARD_NAME=$(whiptail --inputbox "Please enter the Cardholder's First Name" 8 78 --title "Cardholder's First Name" 3>&1 1>&2 2>&3)
+		whiptail_input "SMARTCARD_NAME" "SMARTCARD_NAME_TEXT" "SMARTCARD_NAME_TITLE"
 		
-		SMARTCARD_LANG=$(whiptail --inputbox "Please enter the Preferred Language. E.g. en, fr, de" 8 78 en --title "Cardholder's Preferred Language" 3>&1 1>&2 2>&3)
+		whiptail_input "SMARTCARD_LANG" "SMARTCARD_LANG_TEXT" "SMARTCARD_LANG_TITLE" 
 		
-		SMARTCARD_LOGIN=$(whiptail --inputbox "Please enter the Cardholder's Login Name" 8 78 --title "Cardholder's Login" 3>&1 1>&2 2>&3)
+		whiptail_input "SMARTCARD_LOGIN" "SMARTCARD_LOGIN_TEXT" "SMARTCARD_LOGIN_TITLE"
 	fi
 
 	# SET ADMIN PIN 
-	if (whiptail --yesno "Would you like to set/change the Admin PIN of your smartcard?" 8 78 --title "Change Admin PIN") then
+	if whiptail_yesno "ADMIN_PIN_YESNO_TEXT" "ADMIN_PIN_YESNO_TITLE"; then
 		while true
 		do
-			OLD_ADMIN=$(whiptail --passwordbox "Please enter the old Admin PIN" 8 78 12345678 --title "Old Admin PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "OLD_ADMIN" "OLD_ADMIN_PIN_TEXT" "OLD_ADMIN_PIN_TITLE"
 
-			CONF_OLD_ADMIN=$(whiptail --passwordbox "Please re-enter your old Admin PIN" 8 78 12345678 --title "Verify Old Admin PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "OLD_ADMIN_CONF" "OLD_ADMIN_PIN_CONF_TEXT" "OLD_ADMIN_PIN_CONF_TITLE"
 
-			[ $OLD_ADMIN = $CONF_OLD_ADMIN ] && break
+			[ $OLD_ADMIN = $OLD_ADMIN_CONF ] && break
 		done
 
 		while true
 		do
-			ADMIN=$(whiptail --passwordbox "Please enter the new Admin PIN" 8 78 --title "New Admin PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "ADMIN" "NEW_ADMIN_PIN_TEXT" "NEW_ADMIN_PIN_TITLE"
 		
-			CONF_ADMIN=$(whiptail --passwordbox "Please re-enter the new Admin PIN" 8 78 --title "Verify New Admin PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "ADMIN_CONF" "NEW_ADMIN_PIN_CONF_TEXT" "OLD_ADMIN_PIN_CONF_TITLE"
 
-			[ $ADMIN = $CONF_ADMIN ] && break
+			[ $ADMIN = $ADMIN_CONF ] && break
 		done
 	fi
 
 	# SET USER PIN
-	if (whiptail --yesno "Would you like to set/change the User PIN on your smartcard?" 8 78 --title "Change User PIN") then
+	if whiptail_yesno "USER_PIN_YESNO_TEXT" "USER_PIN_YESNO_TITLE"; then
 		while true
 		do
-			OLD_PIN=$(whiptail --passwordbox "Please enter the old User PIN" 8 78 123456 --title "Old User PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "OLD_PIN" "OLD_USER_PIN_TEXT" "OLD_USER_PIN_TITLE"
 
-			CONF_OLD_PIN=$(whiptail --passwordbox "Please re-enter your old User PIN" 8 78 123456 --title "Verify Old User PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "OLD_PIN_CONF" "OLD_USER_PIN_CONF_TEXT" "OLD_USER_PIN_CONF_TITLE"
 
-			[ $OLD_PIN = $CONF_OLD_PIN ] && break
+			[ $OLD_PIN = $OLD_PIN_CONF ] && break
 		done
 
 		while true
 		do
-			PIN=$(whiptail --passwordbox "Please enter the new PIN" 8 78 --title "New PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "PIN" "NEW_USER_PIN_TEXT" "NEW_USER_PIN_TITLE"
 		
-			CONF_PIN=$(whiptail --passwordbox "Please re-enter the new PIN" 8 78 --title "Verify New PIN" 3>&1 1>&2 2>&3)
+			whiptail_password "PIN_CONF" "NEW_USER_PIN_CONF_TEXT" "NEW_USER_PIN_CONF_TITLE"
 
-			[ $PIN = $CONF_PIN ] && break
+			[ $PIN = $PIN_CONF ] && break
 		done
 	fi
 
 	# SET RESET CODE
-	if (whiptail --yesno "Would you like to set the reset code for your smartcard?" 8 78 --title "Set Reset Code") then
+	if whiptail_yesno "RESET_CODE_YESNO_TEXT" "RESET_CODE_YESNO_TITLE"; then
 		while true
 		do
-			RESET=$(whiptail --passwordbox "Please enter the new Reset Code" 8 78 --title "New Reset Code" 3>&1 1>&2 2>&3)
+			whiptail_password "RESET" "RESET_CODE_TEXT" "RESET_CODE_TITLE"
 
-			CONF_RESET=$(whiptail --passwordbox "Please re-enter the new Reset Code" 8 78 --title "Verify New Reset Code" 3>&1 1>&2 2>&3)
+			whiptail_password "CONF_RESET" "RESET_CODE_CONF_TEXT" "RESET_CODE_CONF_TITLE"
 
-			[ $RESET = $CONF_RESET ] && break
+			[ $RESET = $RESET_CONF ] && break
 		done
 	fi
 
