beb9e7a5746abeec6181c0682164a3b6c5c2a7ef
Elizabeth Ferdman <gnudevliz@gmail.com>
2017-02-16 15:32:32 -0800

    source files in init script

--- init.sh
+++ init.sh
@@ -5,5 +5,15 @@
 set -e
 set -a 
 
+# set environmental variables for internationalization
 export TEXTDOMAINDIR=$HOME/gpg-helper-scripts/locale
 export TEXTDOMAIN=ui-strings
+
+# process the .po file into .mo file
+msgfmt -o locale/en_US/LC_MESSAGES/ui-strings.mo po/en.po
+
+# source helper files
+. whiptail/whiptail_helper.sh
+
+# call the main menu
+. whiptail/main_menu.sh
