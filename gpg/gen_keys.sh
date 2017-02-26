ebfad57bc68ffe5500e9761f695ac80caf28acaa
Elizabeth Ferdman <gnudevliz@gmail.com>
2017-02-25 19:09:38 -0800

    change gpg2 to gpg

--- gpg/gen_keys.sh
+++ gpg/gen_keys.sh
@@ -14,7 +14,7 @@ set -e
 . whiptail/gen_keys.sh
 
 echo "Generating the master key..."
-gpg2 --batch --passphrase $PASSWORD --quick-gen-key $PRIMARY_UID $MASTER_KEY_ALGO - $MASTER_EXPIRY
+gpg --batch --passphrase $PASSWORD --quick-gen-key $PRIMARY_UID $MASTER_KEY_ALGO - $MASTER_EXPIRY
 
 # Set the primary UID
 ./set_primary_uid.sh
@@ -23,23 +23,23 @@ echo "Adding any additional UIDs..."
 python add-uids.py
 
 echo "Generating the secondary key..."
-FPR=$(gpg2 --with-colons --fingerprint | awk -F: '$1 == "fpr" {print $10;}')
-gpg2 --quick-addkey $FPR $SUBKEY_ALGO encrypt $SUBKEY_EXPIRY
+FPR=$(gpg --with-colons --fingerprint | awk -F: '$1 == "fpr" {print $10;}')
+gpg --quick-addkey $FPR $SUBKEY_ALGO encrypt $SUBKEY_EXPIRY
 
 echo "Generating additional signing, encryption and authentication subkeys"
 if ! [ -z $SIGN_SUBKEY ];
 then
-	gpg2 --quick-addkey $FPR $SIGN_SUBKEY sign $SIGN_SUBKEY_EXPIRY;
+	gpg --quick-addkey $FPR $SIGN_SUBKEY sign $SIGN_SUBKEY_EXPIRY;
 fi
 
 if ! [ -z $ENCR_SUBKEY ];
 then
-	gpg2 --quick-addkey $FPR $ENCR_SUBKEY encrypt $ENCR_SUBKEY_EXPIRY;
+	gpg --quick-addkey $FPR $ENCR_SUBKEY encrypt $ENCR_SUBKEY_EXPIRY;
 fi
 
 if ! [ -z $AUTH_SUBKEY ];
 then
-	gpg2 --quick-addkey $FPR $AUTH_SUBKEY auth $AUTH_SUBKEY_EXPIRY;
+	gpg --quick-addkey $FPR $AUTH_SUBKEY auth $AUTH_SUBKEY_EXPIRY;
 fi
 
 # Print the Paper Key time stamped with fingerprint.
--- gpg/gen_primary_key.sh
+++ gpg/gen_primary_key.sh
@@ -4,11 +4,10 @@
 
 set -e
 
-# Grab the user's information and preferences
-. whiptail/gen_keys.sh
-
 echo "Generating the master key..."
-gpg2 --batch --passphrase $PASSWORD --quick-gen-key $PRIMARY_UID $MASTER_KEY_ALGO - $MASTER_EXPIRY
+gpg --batch --passphrase $PASSWORD \
+--quick-gen-key $PRIMARY_UID \
+$MASTER_KEY_ALGO - $MASTER_EXPIRY
 
 echo "Adding any additional UIDs..."
 python add-uids.py
