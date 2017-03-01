fea8c3df491f2de82d706ac33620d1b8145b97dc
Elizabeth Ferdman <gnudevliz@gmail.com>
2017-02-28 22:32:58 -0800

    change gpg2 to gpg

--- gpg/add_uids.py
+++ gpg/add_uids.py
@@ -6,10 +6,10 @@ import os
 
 primary_uid = os.environ["PRIMARY_UID"]
 uid_string = os.environ["ADD_UIDS"]
-gpg_adduid = "gpg2 --quick-adduid " + primary_uid + " " + uids[i]
+gpg_adduid = "gpg --quick-adduid " + primary_uid + " " + uids[i]
 
 # Set the first UID as the Primary UID before adding more
-os.system("echo 'uid 1\nprimary\nsave\n' | gpg2 --command-fd 0 --edit-key " + primary_uid)
+os.system("echo 'uid 1\nprimary\nsave\n' | gpg --command-fd 0 --edit-key " + primary_uid)
 
 # Add each uid
 uids = uid_string.split(", ")
