7639849a236be7ee46e9941834da0f3ca88137ec
Elizabeth Ferdman <gnudevliz@gmail.com>
2017-02-28 22:36:27 -0800

    use default parameter

--- gpg/gen_primary_key.sh
+++ gpg/gen_primary_key.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #
-# Generate primary and subkeys
+# Generate primary key
 
 set -e
+
+MASTER_KEY_USAGE=${1:--} 
 
 echo "Generating the master key..."
 gpg --batch --passphrase $PASSWORD \
 --quick-gen-key $PRIMARY_UID \
-$MASTER_KEY_ALGO - $MASTER_EXPIRY
+$MASTER_KEY_ALGO $MASTER_KEY_USAGE $MASTER_EXPIRY
 
 echo "Adding any additional UIDs..."
 python add-uids.py
